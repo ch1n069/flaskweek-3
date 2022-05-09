a69b150f165446287eaa8390c9b990f16ac42704
Moringa School <brunolalpachino@gmail.com>
2022-05-09 10:45:40 +0300

    added the registration form classes

--- app/views.py
+++ app/views.py
@@ -1,11 +1,9 @@
-import sys
 
 # the sys method is to help us go into the folder of auth using the prepend method
 
 from flask import render_template,redirect,url_for,abort
 from app import app
-sys.path.append("/home/moringa/moringa_pre-prep/flask/pitch-app/app/auth")
-from forms import RegistrationForm , LoginForm  
+from app.auth.forms import RegistrationForm, LoginForm  
 
 
 
@@ -23,8 +21,9 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/register')
+@app.route('/register' , methods=['GET','POST'])
 def register():
+    if form.validate_on
     forms = RegistrationForm()
     return render_template('register.html', title='Register', forms=forms)
     
@@ -33,4 +32,3 @@ def register():
 def login():
     forms = LoginForm()
     return render_template('login.html', title='login', forms=forms)
-    
