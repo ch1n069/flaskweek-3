08d2109f72c471bec47a944d5248f36373d57b5a
Moringa School <brunolalpachino@gmail.com>
2022-05-09 14:11:20 +0300

    added git ignore file

--- app/models.py
+++ app/models.py
@@ -1,1 +1,14 @@
+from flask_sqlalchemy import SQLAlchemy
+#MOdel for comments or reviews
+#models for users 
+# model for categories
+#model for votes
+# model for pitches
 
+
+db = SQLAlchemy(app)
+
+# users models  
+
+
+class Users(db.Model):
